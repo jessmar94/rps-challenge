bcb8593b2faec8ae80f124c0c74d42575541d9cc
Jessica Marais <jessica_marais@hotmail.com>
2019-11-25 09:38:03 +0000

    Each player has points score

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,12 @@
 class Player
 
-  attr_reader :name
+  attr_reader :name, :points 
   attr_accessor :choice
 
   def initialize(name)
     @name = name
     @choice = nil
+    @points = 0
   end
 
 end
